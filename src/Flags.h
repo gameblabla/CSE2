88ed2d6eabd4067037f9cd36fed7557f9b82a448
Clownacy <Clownacy@users.noreply.github.com>
2019-02-21 20:20:13 +0000

    Made Flags.cpp ASM-accurate

--- src/Flags.h
+++ src/Flags.h
@@ -1,15 +1,15 @@
 #pragma once
 
-#include <stdint.h>
+#include "WindowsWrapper.h"
 
-extern uint8_t gFlagNPC[1000];
-extern uint8_t gSkipFlag[0x40];
+extern unsigned char gFlagNPC[1000];
+extern unsigned char gSkipFlag[0x40];
 
 void InitFlags();
 void InitSkipFlags();
-void SetNPCFlag(int a);
-void CutNPCFlag(int a);
-bool GetNPCFlag(int a);
-void SetSkipFlag(int a);
-void CutSkipFlag(int a);
-bool GetSkipFlag(int a);
+void SetNPCFlag(long a);
+void CutNPCFlag(long a);
+BOOL GetNPCFlag(long a);
+void SetSkipFlag(long a);
+void CutSkipFlag(long a);
+BOOL GetSkipFlag(long a);
