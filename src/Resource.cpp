d2f1b2c1a9f9afe74bdb0e9ad79f73c12e28cdb9
Clownacy <Clownacy@users.noreply.github.com>
2019-05-04 18:26:41 +0100

    Renamed some of the resource files
    The Linux port's get_resource function seems to be using the original
    filenames, rather than the 8.3 filenames used by the Windows EXE's
    resource system. Too bad it doesn't tell us the original file extensions.

--- src/Resource.cpp
+++ src/Resource.cpp
@@ -8,70 +8,70 @@
 
 #include "WindowsWrapper.h"
 
-#include "Resource/ORG/ACCESS.org.h"
-#include "Resource/ORG/ANZEN.org.h"
-#include "Resource/ORG/BALCONY.org.h"
-#include "Resource/ORG/BALLOS.org.h"
-#include "Resource/ORG/BDOWN.org.h"
-#include "Resource/ORG/CEMETERY.org.h"
-#include "Resource/ORG/CURLY.org.h"
-#include "Resource/ORG/DR.org.h"
-#include "Resource/ORG/ENDING.org.h"
-#include "Resource/ORG/ESCAPE.org.h"
-#include "Resource/ORG/FANFALE1.org.h"
-#include "Resource/ORG/FANFALE2.org.h"
-#include "Resource/ORG/FANFALE3.org.h"
-#include "Resource/ORG/FIREEYE.org.h"
-#include "Resource/ORG/GAMEOVER.org.h"
-#include "Resource/ORG/GINSUKE.org.h"
-#include "Resource/ORG/GRAND.org.h"
-#include "Resource/ORG/GRAVITY.org.h"
-#include "Resource/ORG/HELL.org.h"
-#include "Resource/ORG/IRONH.org.h"
-#include "Resource/ORG/JENKA.org.h"
-#include "Resource/ORG/JENKA2.org.h"
-#include "Resource/ORG/KODOU.org.h"
-#include "Resource/ORG/LASTBT3.org.h"
-#include "Resource/ORG/LASTBTL.org.h"
-#include "Resource/ORG/LASTCAVE.org.h"
-#include "Resource/ORG/MARINE.org.h"
-#include "Resource/ORG/MAZE.org.h"
-#include "Resource/ORG/MDOWN2.org.h"
-#include "Resource/ORG/MURA.org.h"
-#include "Resource/ORG/OSIDE.org.h"
-#include "Resource/ORG/PLANT.org.h"
-#include "Resource/ORG/QUIET.org.h"
-#include "Resource/ORG/REQUIEM.org.h"
-#include "Resource/ORG/TOROKO.org.h"
-#include "Resource/ORG/VIVI.org.h"
-#include "Resource/ORG/WANPAK2.org.h"
-#include "Resource/ORG/WANPAKU.org.h"
-#include "Resource/ORG/WEED.org.h"
-#include "Resource/ORG/WHITE.org.h"
+#include "Resource/ORG/Access.org.h"
+#include "Resource/ORG/Anzen.org.h"
+#include "Resource/ORG/Balcony.org.h"
+#include "Resource/ORG/Ballos.org.h"
+#include "Resource/ORG/BreakDown.org.h"
+#include "Resource/ORG/Cemetery.org.h"
+#include "Resource/ORG/Curly.org.h"
+#include "Resource/ORG/Dr.org.h"
+#include "Resource/ORG/Ending.org.h"
+#include "Resource/ORG/Escape.org.h"
+#include "Resource/ORG/Fanfale1.org.h"
+#include "Resource/ORG/Fanfale2.org.h"
+#include "Resource/ORG/Fanfale3.org.h"
+#include "Resource/ORG/FireEye.org.h"
+#include "Resource/ORG/Gameover.org.h"
+#include "Resource/ORG/Ginsuke.org.h"
+#include "Resource/ORG/Grand.org.h"
+#include "Resource/ORG/Gravity.org.h"
+#include "Resource/ORG/Hell.org.h"
+#include "Resource/ORG/ironH.org.h"
+#include "Resource/ORG/Jenka.org.h"
+#include "Resource/ORG/Jenka2.org.h"
+#include "Resource/ORG/Kodou.org.h"
+#include "Resource/ORG/LastBtl3.org.h"
+#include "Resource/ORG/LastBtl.org.h"
+#include "Resource/ORG/LastCave.org.h"
+#include "Resource/ORG/Marine.org.h"
+#include "Resource/ORG/Maze.org.h"
+#include "Resource/ORG/MDown2.org.h"
+#include "Resource/ORG/Mura.org.h"
+#include "Resource/ORG/Oside.org.h"
+#include "Resource/ORG/Plant.org.h"
+#include "Resource/ORG/quiet.org.h"
+#include "Resource/ORG/Requiem.org.h"
+#include "Resource/ORG/Toroko.org.h"
+#include "Resource/ORG/Vivi.org.h"
+#include "Resource/ORG/Wanpak2.org.h"
+#include "Resource/ORG/Wanpaku.org.h"
+#include "Resource/ORG/Weed.org.h"
+#include "Resource/ORG/White.org.h"
 #include "Resource/ORG/XXXX.org.h"
-#include "Resource/ORG/ZONBIE.org.h"
-#include "Resource/WAVE/WAVE100.h"
-#include "Resource/BITMAP/CREDIT01.bmp.h"
-#include "Resource/BITMAP/CREDIT02.bmp.h"
-#include "Resource/BITMAP/CREDIT03.bmp.h"
-#include "Resource/BITMAP/CREDIT04.bmp.h"
-#include "Resource/BITMAP/CREDIT05.bmp.h"
-#include "Resource/BITMAP/CREDIT06.bmp.h"
-#include "Resource/BITMAP/CREDIT07.bmp.h"
-#include "Resource/BITMAP/CREDIT08.bmp.h"
-#include "Resource/BITMAP/CREDIT09.bmp.h"
-#include "Resource/BITMAP/CREDIT10.bmp.h"
-#include "Resource/BITMAP/CREDIT11.bmp.h"
-#include "Resource/BITMAP/CREDIT12.bmp.h"
-#include "Resource/BITMAP/CREDIT14.bmp.h"
-#include "Resource/BITMAP/CREDIT15.bmp.h"
-#include "Resource/BITMAP/CREDIT16.bmp.h"
-#include "Resource/BITMAP/CREDIT17.bmp.h"
-#include "Resource/BITMAP/CREDIT18.bmp.h"
+#include "Resource/ORG/Zonbie.org.h"
+#include "Resource/WAVE/Wave.h"
+#include "Resource/BITMAP/Credit01.bmp.h"
+#include "Resource/BITMAP/Credit02.bmp.h"
+#include "Resource/BITMAP/Credit03.bmp.h"
+#include "Resource/BITMAP/Credit04.bmp.h"
+#include "Resource/BITMAP/Credit05.bmp.h"
+#include "Resource/BITMAP/Credit06.bmp.h"
+#include "Resource/BITMAP/Credit07.bmp.h"
+#include "Resource/BITMAP/Credit08.bmp.h"
+#include "Resource/BITMAP/Credit09.bmp.h"
+#include "Resource/BITMAP/Credit10.bmp.h"
+#include "Resource/BITMAP/Credit11.bmp.h"
+#include "Resource/BITMAP/Credit12.bmp.h"
+#include "Resource/BITMAP/Credit14.bmp.h"
+#include "Resource/BITMAP/Credit15.bmp.h"
+#include "Resource/BITMAP/Credit16.bmp.h"
+#include "Resource/BITMAP/Credit17.bmp.h"
+#include "Resource/BITMAP/Credit18.bmp.h"
 #ifdef JAPANESE
-#include "Resource/BITMAP/PIXEL_JP.bmp.h"
+#include "Resource/BITMAP/pixel_jp.bmp.h"
 #else
-#include "Resource/BITMAP/PIXEL.bmp.h"
+#include "Resource/BITMAP/pixel.bmp.h"
 #endif
 #ifndef WINDOWS
 #include "Resource/ICON/ICON_MINI.bmp.h"
@@ -84,203 +84,203 @@ const unsigned char* GetResource(const char *name, size_t *size)
 	//ORG
 	if (!strcmp(name, "ACCESS"))
 	{
-		*size = sizeof(rACCESS);
-		return rACCESS;
+		*size = sizeof(rAccess);
+		return rAccess;
 	}
 	if (!strcmp(name, "ANZEN"))
 	{
-		*size = sizeof(rANZEN);
-		return rANZEN;
+		*size = sizeof(rAnzen);
+		return rAnzen;
 	}
 	if (!strcmp(name, "BALCONY"))
 	{
-		*size = sizeof(rBALCONY);
-		return rBALCONY;
+		*size = sizeof(rBalcony);
+		return rBalcony;
 	}
 	if (!strcmp(name, "BALLOS"))
 	{
-		*size = sizeof(rBALLOS);
-		return rBALLOS;
+		*size = sizeof(rBallos);
+		return rBallos;
 	}
 	if (!strcmp(name, "BDOWN"))
 	{
-		*size = sizeof(rBDOWN);
-		return rBDOWN;
+		*size = sizeof(rBreakDown);
+		return rBreakDown;
 	}
 	if (!strcmp(name, "CEMETERY"))
 	{
-		*size = sizeof(rCEMETERY);
-		return rCEMETERY;
+		*size = sizeof(rCemetery);
+		return rCemetery;
 	}
 	if (!strcmp(name, "CURLY"))
 	{
-		*size = sizeof(rCURLY);
-		return rCURLY;
+		*size = sizeof(rCurly);
+		return rCurly;
 	}
 	if (!strcmp(name, "DR"))
 	{
-		*size = sizeof(rDR);
-		return rDR;
+		*size = sizeof(rDr);
+		return rDr;
 	}
 	if (!strcmp(name, "ENDING"))
 	{
-		*size = sizeof(rENDING);
-		return rENDING;
+		*size = sizeof(rEnding);
+		return rEnding;
 	}
 	if (!strcmp(name, "ESCAPE"))
 	{
-		*size = sizeof(rESCAPE);
-		return rESCAPE;
+		*size = sizeof(rEscape);
+		return rEscape;
 	}
 	if (!strcmp(name, "FANFALE1"))
 	{
-		*size = sizeof(rFANFALE1);
-		return rFANFALE1;
+		*size = sizeof(rFanfale1);
+		return rFanfale1;
 	}
 	if (!strcmp(name, "FANFALE2"))
 	{
-		*size = sizeof(rFANFALE2);
-		return rFANFALE2;
+		*size = sizeof(rFanfale2);
+		return rFanfale2;
 	}
 	if (!strcmp(name, "FANFALE3"))
 	{
-		*size = sizeof(rFANFALE3);
-		return rFANFALE3;
+		*size = sizeof(rFanfale3);
+		return rFanfale3;
 	}
 	if (!strcmp(name, "FIREEYE"))
 	{
-		*size = sizeof(rFIREEYE);
-		return rFIREEYE;
+		*size = sizeof(rFireEye);
+		return rFireEye;
 	}
 	if (!strcmp(name, "GAMEOVER"))
 	{
-		*size = sizeof(rGAMEOVER);
-		return rGAMEOVER;
+		*size = sizeof(rGameover);
+		return rGameover;
 	}
 	if (!strcmp(name, "GINSUKE"))
 	{
-		*size = sizeof(rGINSUKE);
-		return rGINSUKE;
+		*size = sizeof(rGinsuke);
+		return rGinsuke;
 	}
 	if (!strcmp(name, "GRAND"))
 	{
-		*size = sizeof(rGRAND);
-		return rGRAND;
+		*size = sizeof(rGrand);
+		return rGrand;
 	}
 	if (!strcmp(name, "GRAVITY"))
 	{
-		*size = sizeof(rGRAVITY);
-		return rGRAVITY;
+		*size = sizeof(rGravity);
+		return rGravity;
 	}
 	if (!strcmp(name, "HELL"))
 	{
-		*size = sizeof(rHELL);
-		return rHELL;
+		*size = sizeof(rHell);
+		return rHell;
 	}
 	if (!strcmp(name, "IRONH"))
 	{
-		*size = sizeof(rIRONH);
-		return rIRONH;
+		*size = sizeof(rironH);
+		return rironH;
 	}
 	if (!strcmp(name, "JENKA"))
 	{
-		*size = sizeof(rJENKA);
-		return rJENKA;
+		*size = sizeof(rJenka);
+		return rJenka;
 	}
 	if (!strcmp(name, "JENKA2"))
 	{
-		*size = sizeof(rJENKA2);
-		return rJENKA2;
+		*size = sizeof(rJenka2);
+		return rJenka2;
 	}
 	if (!strcmp(name, "KODOU"))
 	{
-		*size = sizeof(rKODOU);
-		return rKODOU;
+		*size = sizeof(rKodou);
+		return rKodou;
 	}
 	if (!strcmp(name, "LASTBT3"))
 	{
-		*size = sizeof(rLASTBT3);
-		return rLASTBT3;
+		*size = sizeof(rLastBtl3);
+		return rLastBtl3;
 	}
 	if (!strcmp(name, "LASTBTL"))
 	{
-		*size = sizeof(rLASTBTL);
-		return rLASTBTL;
+		*size = sizeof(rLastBtl);
+		return rLastBtl;
 	}
 	if (!strcmp(name, "LASTCAVE"))
 	{
-		*size = sizeof(rLASTCAVE);
-		return rLASTCAVE;
+		*size = sizeof(rLastCave);
+		return rLastCave;
 	}
 	if (!strcmp(name, "MARINE"))
 	{
-		*size = sizeof(rMARINE);
-		return rMARINE;
+		*size = sizeof(rMarine);
+		return rMarine;
 	}
 	if (!strcmp(name, "MAZE"))
 	{
-		*size = sizeof(rMAZE);
-		return rMAZE;
+		*size = sizeof(rMaze);
+		return rMaze;
 	}
 	if (!strcmp(name, "MDOWN2"))
 	{
-		*size = sizeof(rMDOWN2);
-		return rMDOWN2;
+		*size = sizeof(rMDown2);
+		return rMDown2;
 	}
 	if (!strcmp(name, "MURA"))
 	{
-		*size = sizeof(rMURA);
-		return rMURA;
+		*size = sizeof(rMura);
+		return rMura;
 	}
 	if (!strcmp(name, "OSIDE"))
 	{
-		*size = sizeof(rOSIDE);
-		return rOSIDE;
+		*size = sizeof(rOside);
+		return rOside;
 	}
 	if (!strcmp(name, "PLANT"))
 	{
-		*size = sizeof(rPLANT);
-		return rPLANT;
+		*size = sizeof(rPlant);
+		return rPlant;
 	}
 	if (!strcmp(name, "QUIET"))
 	{
-		*size = sizeof(rQUIET);
-		return rQUIET;
+		*size = sizeof(rquiet);
+		return rquiet;
 	}
 	if (!strcmp(name, "REQUIEM"))
 	{
-		*size = sizeof(rREQUIEM);
-		return rREQUIEM;
+		*size = sizeof(rRequiem);
+		return rRequiem;
 	}
 	if (!strcmp(name, "TOROKO"))
 	{
-		*size = sizeof(rTOROKO);
-		return rTOROKO;
+		*size = sizeof(rToroko);
+		return rToroko;
 	}
 	if (!strcmp(name, "VIVI"))
 	{
-		*size = sizeof(rVIVI);
-		return rVIVI;
+		*size = sizeof(rVivi);
+		return rVivi;
 	}
 	if (!strcmp(name, "WANPAK2"))
 	{
-		*size = sizeof(rWANPAK2);
-		return rWANPAK2;
+		*size = sizeof(rWanpak2);
+		return rWanpak2;
 	}
 	if (!strcmp(name, "WANPAKU"))
 	{
-		*size = sizeof(rWANPAKU);
-		return rWANPAKU;
+		*size = sizeof(rWanpaku);
+		return rWanpaku;
 	}
 	if (!strcmp(name, "WEED"))
 	{
-		*size = sizeof(rWEED);
-		return rWEED;
+		*size = sizeof(rWeed);
+		return rWeed;
 	}
 	if (!strcmp(name, "WHITE"))
 	{
-		*size = sizeof(rWHITE);
-		return rWHITE;
+		*size = sizeof(rWhite);
+		return rWhite;
 	}
 	if (!strcmp(name, "XXXX"))
 	{
@@ -289,111 +289,111 @@ const unsigned char* GetResource(const char *name, size_t *size)
 	}
 	if (!strcmp(name, "ZONBIE"))
 	{
-		*size = sizeof(rZONBIE);
-		return rZONBIE;
+		*size = sizeof(rZonbie);
+		return rZonbie;
 	}
 	
 	//WAVE
 	if (!strcmp(name, "WAVE100"))
 	{
-		*size = sizeof(rWAVE100);
-		return rWAVE100;
+		*size = sizeof(rWave);
+		return rWave;
 	}
 	
 	//Bitmap
 	if (!strcmp(name, "CREDIT01"))
 	{
-		*size = sizeof(rCREDIT01);
-		return rCREDIT01;
+		*size = sizeof(rCredit01);
+		return rCredit01;
 	}
 	if (!strcmp(name, "CREDIT02"))
 	{
-		*size = sizeof(rCREDIT02);
-		return rCREDIT02;
+		*size = sizeof(rCredit02);
+		return rCredit02;
 	}
 	if (!strcmp(name, "CREDIT03"))
 	{
-		*size = sizeof(rCREDIT03);
-		return rCREDIT03;
+		*size = sizeof(rCredit03);
+		return rCredit03;
 	}
 	if (!strcmp(name, "CREDIT04"))
 	{
-		*size = sizeof(rCREDIT04);
-		return rCREDIT04;
+		*size = sizeof(rCredit04);
+		return rCredit04;
 	}
 	if (!strcmp(name, "CREDIT05"))
 	{
-		*size = sizeof(rCREDIT05);
-		return rCREDIT05;
+		*size = sizeof(rCredit05);
+		return rCredit05;
 	}
 	if (!strcmp(name, "CREDIT06"))
 	{
-		*size = sizeof(rCREDIT06);
-		return rCREDIT06;
+		*size = sizeof(rCredit06);
+		return rCredit06;
 	}
 	if (!strcmp(name, "CREDIT07"))
 	{
-		*size = sizeof(rCREDIT07);
-		return rCREDIT07;
+		*size = sizeof(rCredit07);
+		return rCredit07;
 	}
 	if (!strcmp(name, "CREDIT08"))
 	{
-		*size = sizeof(rCREDIT08);
-		return rCREDIT08;
+		*size = sizeof(rCredit08);
+		return rCredit08;
 	}
 	if (!strcmp(name, "CREDIT09"))
 	{
-		*size = sizeof(rCREDIT09);
-		return rCREDIT09;
+		*size = sizeof(rCredit09);
+		return rCredit09;
 	}
 	if (!strcmp(name, "CREDIT10"))
 	{
-		*size = sizeof(rCREDIT10);
-		return rCREDIT10;
+		*size = sizeof(rCredit10);
+		return rCredit10;
 	}
 	if (!strcmp(name, "CREDIT11"))
 	{
-		*size = sizeof(rCREDIT11);
-		return rCREDIT11;
+		*size = sizeof(rCredit11);
+		return rCredit11;
 	}
 	if (!strcmp(name, "CREDIT12"))
 	{
-		*size = sizeof(rCREDIT12);
-		return rCREDIT12;
+		*size = sizeof(rCredit12);
+		return rCredit12;
 	}
 	if (!strcmp(name, "CREDIT14"))
 	{
-		*size = sizeof(rCREDIT14);
-		return rCREDIT14;
+		*size = sizeof(rCredit14);
+		return rCredit14;
 	}
 	if (!strcmp(name, "CREDIT15"))
 	{
-		*size = sizeof(rCREDIT15);
-		return rCREDIT15;
+		*size = sizeof(rCredit15);
+		return rCredit15;
 	}
 	if (!strcmp(name, "CREDIT16"))
 	{
-		*size = sizeof(rCREDIT16);
-		return rCREDIT16;
+		*size = sizeof(rCredit16);
+		return rCredit16;
 	}
 	if (!strcmp(name, "CREDIT17"))
 	{
-		*size = sizeof(rCREDIT17);
-		return rCREDIT17;
+		*size = sizeof(rCredit17);
+		return rCredit17;
 	}
 	if (!strcmp(name, "CREDIT18"))
 	{
-		*size = sizeof(rCREDIT18);
-		return rCREDIT18;
+		*size = sizeof(rCredit18);
+		return rCredit18;
 	}
 	if (!strcmp(name, "PIXEL"))
 	{
 #ifdef JAPANESE
-		*size = sizeof(rPIXEL_JP);
-		return rPIXEL_JP;
+		*size = sizeof(rpixel_jp);
+		return rpixel_jp;
 #else
-		*size = sizeof(rPIXEL);
-		return rPIXEL;
+		*size = sizeof(rpixel);
+		return rpixel;
 #endif
 	}
 	
