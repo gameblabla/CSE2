58a3974e1a648a235a136aa2249c2bab22ed3e91
Clownacy <Clownacy@users.noreply.github.com>
2019-05-04 21:39:16 +0000

    Added an accuracy improvement
    It affects portability though, so it's hidden behind the NONPORTABLE
    flag.

--- src/Stage.cpp
+++ src/Stage.cpp
@@ -234,7 +234,11 @@ const char *gMusicTable[42] =
 	"ACCESS",
 	"IRONH",
 	"GRAND",
+#ifdef NONPORTABLE
+	"Curly",	// The vanilla game used the original filename instead of the internal 8.3 one
+#else
 	"CURLY",
+#endif
 	"OSIDE",
 	"REQUIEM",
 	"WANPAK2",
