27d9dcfa33479cec82b1ccdfe26739be3b7d610a
Clownacy <Clownacy@users.noreply.github.com>
2019-07-16 13:35:32 +0100

    'Power', not 'multiple'

--- src/CommonDefines.h
+++ src/CommonDefines.h
@@ -5,4 +5,4 @@
 #define WINDOW_WIDTH 426
 #define WINDOW_HEIGHT 240
 
-#define SPRITE_SCALE 1	// Set to 2 for 2x sprites, 4 for 4x, etc. Must be a multiple of 2
+#define SPRITE_SCALE 1	// Set to 2 for 2x sprites, 4 for 4x, etc. Must be a power of 2
