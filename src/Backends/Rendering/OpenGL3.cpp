4769397c7ac45c074e88208dcefd585c7c8f3c63
Clownacy <Clownacy@users.noreply.github.com>
2019-07-31 23:03:32 +0000

    Clean up OpenGL error callback print

--- src/Backends/Rendering/OpenGL3.cpp
+++ src/Backends/Rendering/OpenGL3.cpp
@@ -121,8 +121,14 @@ void main() \
 
 static void GLAPIENTRY MessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *message, const void* userParam)
 {
+	(void)source;
+	(void)id;
+	(void)severity;
+	(void)length;
+	(void)userParam;
+
 	if (type == GL_DEBUG_TYPE_ERROR)
-		printf("GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n", ( type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : "" ), type, severity, message);
+		printf("OpenGL error: %s\n", message);
 }
 
 static GLuint CompileShader(const char *vertex_shader_source, const char *fragment_shader_source)
