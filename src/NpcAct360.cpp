e898052a767e05396dc98dd93246b21552ea40af
Clownacy <Clownacy@users.noreply.github.com>
2019-11-02 21:28:06 +0000

    Shut up some warnings

--- src/NpcAct360.cpp
+++ src/NpcAct360.cpp
@@ -5,45 +5,54 @@
 
 void ActNpc361(NPCHAR *npc)
 {
+	(void)npc;
 	// TBD
 }
 
 void ActNpc362(NPCHAR *npc)
 {
+	(void)npc;
 	// TBD
 }
 
 void ActNpc363(NPCHAR *npc)
 {
+	(void)npc;
 	// TBD
 }
 
 void ActNpc364(NPCHAR *npc)
 {
+	(void)npc;
 	// TBD
 }
 
 void ActNpc365(NPCHAR *npc)
 {
+	(void)npc;
 	// TBD
 }
 
 void ActNpc366(NPCHAR *npc)
 {
+	(void)npc;
 	// TBD
 }
 
 void ActNpc367(NPCHAR *npc)
 {
+	(void)npc;
 	// TBD
 }
 
 void ActNpc368(NPCHAR *npc)
 {
+	(void)npc;
 	// TBD
 }
 
 void ActNpc369(NPCHAR *npc)
 {
+	(void)npc;
 	// TBD
 }
