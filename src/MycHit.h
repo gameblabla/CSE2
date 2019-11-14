7df1eb9c58299683141f4a687f7177722d039002
Clownacy <Clownacy@users.noreply.github.com>
2019-11-14 23:58:33 +0000

    Clean-up MycHit.cpp
    Found some ASM-inaccuracies in here too

--- src/MycHit.h
+++ src/MycHit.h
@@ -1,6 +1,6 @@
 #pragma once
 
-void ResetMyCharFlag();
-void HitMyCharMap();
-void HitMyCharNpChar();
-void HitMyCharBoss();
+void ResetMyCharFlag(void);
+void HitMyCharMap(void);
+void HitMyCharNpChar(void);
+void HitMyCharBoss(void);
