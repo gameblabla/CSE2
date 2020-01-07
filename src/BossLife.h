dbe773f01a1221c483643e33bd50d0245d2aa077
Clownacy <Clownacy@users.noreply.github.com>
2020-01-07 04:50:15 +0000

    ...Remove more C-compatibility stuff

--- src/BossLife.h
+++ src/BossLife.h
@@ -2,15 +2,7 @@
 
 #include "WindowsWrapper.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 void InitBossLife(void);
 BOOL StartBossLife(int code_event);
 BOOL StartBossLife2(void);
 void PutBossLife(void);
-
-#ifdef __cplusplus
-}
-#endif
--- src/ValueView.h
+++ src/ValueView.h
@@ -2,10 +2,6 @@
 
 #include "WindowsWrapper.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 typedef struct VALUEVIEW
 {
   BOOL flag;
@@ -21,7 +17,3 @@ void ClearValueView(void);
 void SetValueView(int *px, int *py, int value);
 void ActValueView(void);
 void PutValueView(int flx, int fly);
-
-#ifdef __cplusplus
-}
-#endif
