ed1ff2f275ff2f43cb0cad64cf40a2a032349016
Clownacy <Clownacy@users.noreply.github.com>
2020-01-29 23:45:22 +0000

    Tweak MIN/MAX macros in software renderer

--- src/Backends/Rendering/Software.cpp
+++ src/Backends/Rendering/Software.cpp
@@ -10,10 +10,8 @@
 
 #include "../../Resource.h"
 
-#undef MIN
-#undef MAX
-#define MIN(a,b) ((a) < (b) ? (a) : (b))
-#define MAX(a,b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 typedef struct Backend_Surface
 {
