61aec1cb15220fc52919ce7c43cf0a4d416a4cb5
Clownacy <Clownacy@users.noreply.github.com>
2020-03-17 13:21:37 +0000

    Use native Windows types/macros when available
    This prevents conflicts when a translation unit includes
    `WindowsWrapper.h`, and middleware that includes `windows.h`.

--- src/WindowsWrapper.h
+++ src/WindowsWrapper.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#ifdef _WIN32
+#include <windef.h>
+#include <wingdi.h>
+#else
+
 #include <stdio.h>
 
 #define RGB(r,g,b) ((r) | ((g) << 8) | ((b) << 16))
@@ -18,3 +23,5 @@ struct RECT
 };
 
 #define MAX_PATH FILENAME_MAX
+
+#endif
