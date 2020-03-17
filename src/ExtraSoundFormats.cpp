0a7c3c4149866d8a40911e8c3337ab77fe769414
Clownacy <Clownacy@users.noreply.github.com>
2020-03-17 18:20:51 +0000

    Update clownaudio terminology

--- src/ExtraSoundFormats.cpp
+++ src/ExtraSoundFormats.cpp
@@ -12,8 +12,8 @@
 typedef struct SoundSlot
 {
 	bool valid;
-	ClownAudio_SoundData *sound;
-	ClownAudio_Sound instance;
+	ClownAudio_SoundData *sound_data;
+	ClownAudio_Sound sound;
 } SoundSlot;
 
 static ClownAudio_Mixer *mixer;
@@ -35,15 +35,15 @@ void ExtraSound_Deinit(void)
 	// Free songs
 	if (previous_song.valid)
 	{
-		ClownAudio_DestroySound(mixer, previous_song.instance);
-		ClownAudio_UnloadSoundData(previous_song.sound);
+		ClownAudio_DestroySound(mixer, previous_song.sound);
+		ClownAudio_UnloadSoundData(previous_song.sound_data);
 		previous_song.valid = false;
 	}
 
 	if (song.valid)
 	{
-		ClownAudio_DestroySound(mixer, song.instance);
-		ClownAudio_UnloadSoundData(song.sound);
+		ClownAudio_DestroySound(mixer, song.sound);
+		ClownAudio_UnloadSoundData(song.sound_data);
 		song.valid = false;
 	}
 
@@ -52,8 +52,8 @@ void ExtraSound_Deinit(void)
 	{
 		if (sfx_list[i].valid)
 		{
-			ClownAudio_DestroySound(mixer, sfx_list[i].instance);
-			ClownAudio_UnloadSoundData(sfx_list[i].sound);
+			ClownAudio_DestroySound(mixer, sfx_list[i].sound);
+			ClownAudio_UnloadSoundData(sfx_list[i].sound_data);
 			sfx_list[i].valid = false;
 		}
 	}
@@ -77,12 +77,12 @@ void ExtraSound_LoadMusic(const char *path, bool loop)
 
 	if (previous_song.valid)
 	{
-		ClownAudio_DestroySound(mixer, previous_song.instance);
-		ClownAudio_UnloadSoundData(previous_song.sound);
+		ClownAudio_DestroySound(mixer, previous_song.sound);
+		ClownAudio_UnloadSoundData(previous_song.sound_data);
 	}
 
 	if (song.valid)
-		ClownAudio_PauseSound(mixer, song.instance);
+		ClownAudio_PauseSound(mixer, song.sound);
 
 	previous_song = song;
 
@@ -92,25 +92,25 @@ void ExtraSound_LoadMusic(const char *path, bool loop)
 
 		ClownAudio_SoundDataConfig data_config;
 		ClownAudio_InitSoundDataConfig(&data_config);
-		song.sound = ClownAudio_LoadSoundDataFromFiles(path, NULL, &data_config);
+		song.sound_data = ClownAudio_LoadSoundDataFromFiles(path, NULL, &data_config);
 
-		if (song.sound != NULL)
+		if (song.sound_data != NULL)
 		{
 			ClownAudio_SoundConfig sound_config;
 			ClownAudio_InitSoundConfig(&sound_config);
 			sound_config.loop = loop;
-			song.instance = ClownAudio_CreateSound(mixer, song.sound, &sound_config);
+			song.sound = ClownAudio_CreateSound(mixer, song.sound_data, &sound_config);
 
-			if (song.instance)
+			if (song.sound)
 			{
-				ClownAudio_UnpauseSound(mixer, song.instance);
+				ClownAudio_UnpauseSound(mixer, song.sound);
 
 				song.valid = true;
 
 				return;
 			}
 
-			ClownAudio_UnloadSoundData(song.sound);
+			ClownAudio_UnloadSoundData(song.sound_data);
 		}
 	}
 }
@@ -119,15 +119,15 @@ void ExtraSound_LoadPreviousMusic(void)
 {
 	if (song.valid)
 	{
-		ClownAudio_DestroySound(mixer, song.instance);
-		ClownAudio_UnloadSoundData(song.sound);
+		ClownAudio_DestroySound(mixer, song.sound);
+		ClownAudio_UnloadSoundData(song.sound_data);
 	}
 
 	if (previous_song.valid)
 	{
 		song = previous_song;
-		ClownAudio_CancelFade(mixer, song.instance);
-		ClownAudio_UnpauseSound(mixer, song.instance);
+		ClownAudio_CancelFade(mixer, song.sound);
+		ClownAudio_UnpauseSound(mixer, song.sound);
 	}
 
 	previous_song.valid = false;
@@ -136,20 +136,20 @@ void ExtraSound_LoadPreviousMusic(void)
 void ExtraSound_PauseMusic(void)
 {
 	if (song.valid)
-		ClownAudio_PauseSound(mixer, song.instance);
+		ClownAudio_PauseSound(mixer, song.sound);
 }
 
 void ExtraSound_FadeOutMusic(void)
 {
-	ClownAudio_FadeOutSound(mixer, song.instance, 5 * 1000);
+	ClownAudio_FadeOutSound(mixer, song.sound, 5 * 1000);
 }
 
 void ExtraSound_LoadSFX(const char *path, int id)
 {
 	if (sfx_list[id].valid)
 	{
-		ClownAudio_DestroySound(mixer, sfx_list[id].instance);
-		ClownAudio_UnloadSoundData(sfx_list[id].sound);
+		ClownAudio_DestroySound(mixer, sfx_list[id].sound);
+		ClownAudio_UnloadSoundData(sfx_list[id].sound_data);
 	}
 
 	size_t file_buffer_size;
@@ -157,22 +157,22 @@ void ExtraSound_LoadSFX(const char *path, int id)
 	ClownAudio_SoundDataConfig data_config;
 	ClownAudio_InitSoundDataConfig(&data_config);
 	data_config.predecode = true;
-	sfx_list[id].sound = ClownAudio_LoadSoundDataFromFiles(path, NULL, &data_config);
+	sfx_list[id].sound_data = ClownAudio_LoadSoundDataFromFiles(path, NULL, &data_config);
 
-	if (sfx_list[id].sound != NULL)
+	if (sfx_list[id].sound_data != NULL)
 	{
 		ClownAudio_SoundConfig sound_config;
 		ClownAudio_InitSoundConfig(&sound_config);
 		sound_config.do_not_free_when_done = true;
-		sfx_list[id].instance = ClownAudio_CreateSound(mixer, sfx_list[id].sound, &sound_config);
+		sfx_list[id].sound = ClownAudio_CreateSound(mixer, sfx_list[id].sound_data, &sound_config);
 
-		if (sfx_list[id].instance != 0)
+		if (sfx_list[id].sound != 0)
 		{
 			sfx_list[id].valid = true;
 			return;
 		}
 
-		ClownAudio_UnloadSoundData(sfx_list[id].sound);
+		ClownAudio_UnloadSoundData(sfx_list[id].sound_data);
 	}
 
 	sfx_list[id].valid = false;
@@ -185,19 +185,19 @@ void ExtraSound_PlaySFX(int id, int mode)
 		switch (mode)
 		{
 			case 0:
-				ClownAudio_PauseSound(mixer, sfx_list[id].instance);
+				ClownAudio_PauseSound(mixer, sfx_list[id].sound);
 				break;
 
 			case 1:
-				ClownAudio_PauseSound(mixer, sfx_list[id].instance);
-				ClownAudio_RewindSound(mixer, sfx_list[id].instance);
-				ClownAudio_SetSoundLoop(mixer, sfx_list[id].instance, false);
-				ClownAudio_UnpauseSound(mixer, sfx_list[id].instance);
+				ClownAudio_PauseSound(mixer, sfx_list[id].sound);
+				ClownAudio_RewindSound(mixer, sfx_list[id].sound);
+				ClownAudio_SetSoundLoop(mixer, sfx_list[id].sound, false);
+				ClownAudio_UnpauseSound(mixer, sfx_list[id].sound);
 				break;
 
 			case -1:
-				ClownAudio_SetSoundLoop(mixer, sfx_list[id].instance, true);
-				ClownAudio_UnpauseSound(mixer, sfx_list[id].instance);
+				ClownAudio_SetSoundLoop(mixer, sfx_list[id].sound, true);
+				ClownAudio_UnpauseSound(mixer, sfx_list[id].sound);
 				break;
 		}
 	}
