b4ec82d81b803d7d750cf46c7a5a0c071dea5222
Clownacy <Clownacy@users.noreply.github.com>
2020-03-31 16:38:05 +0100

    Add dummy GLFW3 controller backend

---
+++ src/Backends/Controller/GLFW3.cpp
@@ -0,0 +1,23 @@
+#include "../Controller.h"
+
+#include "../../WindowsWrapper.h"
+
+void ControllerBackend_Deinit(void)
+{
+	
+}
+
+BOOL ControllerBackend_Init(void)
+{
+	return FALSE;
+}
+
+BOOL ControllerBackend_GetJoystickStatus(JOYSTICK_STATUS *status)
+{
+	return FALSE;
+}
+
+BOOL ControllerBackend_ResetJoystickStatus(void)
+{
+	return FALSE;
+}
