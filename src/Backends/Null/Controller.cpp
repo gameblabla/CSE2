f4f85f1f9d7530e9c66d561bbd6941e5f30828c8
Clownacy <Clownacy@users.noreply.github.com>
2020-04-13 18:54:34 +0100

    Add Null platform backend
    Also does absolute nothing.
    
    The point of these things is so it's easier to test out new ports.
    For example, with the Null backends and the software renderer, we
    don't need to write any code to build for other platforms.
    
    In addition, by having no platform-dependant code, this can be used
    to trace crashes. For example, I was having crashes on the Wii U,
    despite there being no Wii U-specific code at all - the cause turned
    out to be that the executable wasn't stripped, and it had nothing to
    do with the code at all.

---
+++ src/Backends/Null/Controller.cpp
@@ -0,0 +1,21 @@
+#include "../Controller.h"
+
+bool ControllerBackend_Init(void)
+{
+	return false;
+}
+
+void ControllerBackend_Deinit(void)
+{
+	
+}
+
+bool ControllerBackend_GetJoystickStatus(bool **buttons, unsigned int *button_count, short **axes, unsigned int *axis_count)
+{
+	(void)buttons;
+	(void)button_count;
+	(void)axes;
+	(void)axis_count;
+
+	return false;
+}
---
+++ src/Backends/Null/Window-Software.cpp
@@ -0,0 +1,39 @@
+#include "../Window-Software.h"
+
+#include <stddef.h>
+#include <stdlib.h>
+
+static unsigned char *framebuffer;
+
+unsigned char* WindowBackend_Software_CreateWindow(const char *window_title, int screen_width, int screen_height, bool fullscreen, size_t *pitch)
+{
+	(void)window_title;
+	(void)fullscreen;
+
+	framebuffer = (unsigned char*)malloc(screen_width * screen_height * 3);
+
+	if (framebuffer != NULL)
+	{
+		*pitch = screen_width * 3;
+
+		return framebuffer;
+	}
+
+	return NULL;
+}
+
+void WindowBackend_Software_DestroyWindow(void)
+{
+	free(framebuffer);
+}
+
+void WindowBackend_Software_Display(void)
+{
+	
+}
+
+void WindowBackend_Software_HandleWindowResize(unsigned int width, unsigned int height)
+{
+	(void)width;
+	(void)height;
+}
