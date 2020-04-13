6d53668beda9b8e88ee6c74230e1e70a2cc0061b
Clownacy <Clownacy@users.noreply.github.com>
2020-04-14 00:31:13 +0100

    Add Wii U platform backend
    With this backend, CSE2 can run on the Wii U.
    
    It's not ideal - it doesn't have its own renderer yet, so it just
    uses the software renderer, and it only displays on the gamepad,
    because drawing to the TV as well makes the game lag.
    
    Also there's no sound.
    
    Also the lack of input rebinding is annoying.

---
+++ src/Backends/WiiU/Misc.cpp
@@ -0,0 +1,114 @@
+#include "../Misc.h"
+
+#include <string.h>
+
+#include <coreinit/thread.h>
+
+#include <whb/proc.h>
+#include <whb/sdcard.h>
+
+static unsigned long tick_delta;
+
+bool Backend_Init(void)
+{
+	WHBProcInit();
+
+	if (!WHBMountSdCard())
+		return FALSE;
+
+	tick_delta = OSGetSystemInfo()->busClockSpeed / 4;
+
+	return true;
+}
+
+void Backend_Deinit(void)
+{
+	WHBUnmountSdCard();
+
+	WHBProcShutdown();
+}
+
+void Backend_PostWindowCreation(void)
+{
+	
+}
+
+bool Backend_GetBasePath(char *string_buffer)
+{
+	strcpy(string_buffer, WHBGetSdCardMountPath());
+	strcat(string_buffer, "/CSE2");
+
+	return true;
+}
+
+void Backend_HideMouse(void)
+{
+	
+}
+
+void Backend_SetWindowIcon(const unsigned char *rgb_pixels, unsigned int width, unsigned int height)
+{
+	(void)rgb_pixels;
+	(void)width;
+	(void)height;
+}
+
+void Backend_SetCursor(const unsigned char *rgb_pixels, unsigned int width, unsigned int height)
+{
+	(void)rgb_pixels;
+	(void)width;
+	(void)height;
+}
+
+void PlaybackBackend_EnableDragAndDrop(void)
+{
+	
+}
+
+bool Backend_SystemTask(bool active)
+{
+	(void)active;
+
+	return WHBProcIsRunning();
+}
+
+void Backend_GetKeyboardState(bool *keyboard_state)
+{
+	memset(keyboard_state, 0, sizeof(bool) * BACKEND_KEYBOARD_TOTAL);
+}
+
+void Backend_ShowMessageBox(const char *title, const char *message)
+{
+	(void)title;
+	(void)message;
+}
+
+ATTRIBUTE_FORMAT_PRINTF(1, 2) void Backend_PrintError(const char *format, ...)
+{
+	(void)format;
+}
+
+ATTRIBUTE_FORMAT_PRINTF(1, 2) void Backend_PrintInfo(const char *format, ...)
+{
+	(void)format;
+}
+
+unsigned long Backend_GetTicks(void)
+{
+	static uint64_t accumulator;
+
+	static unsigned long last_tick;
+
+	unsigned long current_tick = OSGetTick();
+
+	accumulator += current_tick - last_tick;
+
+	last_tick = current_tick;
+
+	return (accumulator * 1000) / tick_delta;
+}
+
+void Backend_Delay(unsigned int ticks)
+{
+	OSSleepTicks((ticks * tick_delta) / 1000);
+}
