f2a82d09bd2bded8f2fc32634cccf7ee862fbf95
Clownacy <Clownacy@users.noreply.github.com>
2020-04-15 20:37:10 +0100

    Remove unneeded `#include`

--- src/Backends/Window-OpenGL.h
+++ src/Backends/Window-OpenGL.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "../WindowsWrapper.h"
-
 bool WindowBackend_OpenGL_CreateWindow(const char *window_title, int *screen_width, int *screen_height, bool fullscreen);
 void WindowBackend_OpenGL_DestroyWindow(void);
 void WindowBackend_OpenGL_Display(void);
