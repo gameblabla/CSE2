43c0b670abfcd539e426a9bbef9220530fabe1b5
Clownacy <Clownacy@users.noreply.github.com>
2020-04-26 00:07:45 +0100

    Figured out the mystery values in RestoreSurfaces
    They're ASCII characters.

--- src/Draw.cpp
+++ src/Draw.cpp
@@ -766,14 +766,14 @@ int RestoreSurfaces(void)
 	{
 		++surfaces_regenerated;
 		frontbuffer->Restore();
-		out(0x66);
+		out('f');	// 'f' for 'frontbuffer'
 	}
 
 	if (backbuffer->IsLost() == DDERR_SURFACELOST)
 	{
 		++surfaces_regenerated;
 		backbuffer->Restore();
-		out(0x62);
+		out('b');	// 'b' for 'backbuffer'
 	}
 
 	for (s = 0; s < SURFACE_ID_MAX; ++s)
@@ -784,7 +784,7 @@ int RestoreSurfaces(void)
 			{
 				++surfaces_regenerated;
 				surf[s]->Restore();
-				out(0x30 + s);
+				out('0' + s);	// The number of the surface lost
 
 				if (!surface_metadata[s].bSystem)
 				{
