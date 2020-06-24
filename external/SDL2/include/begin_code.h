ef7ae860c74643e61196d7206315a44d30422748
Clownacy <Clownacy@users.noreply.github.com>
2020-06-24 17:57:21 +0100

    Update built-in SDL2 to 2.0.12

--- external/SDL2/include/begin_code.h
+++ external/SDL2/include/begin_code.h
@@ -1,6 +1,6 @@
 /*
   Simple DirectMedia Layer
-  Copyright (C) 1997-2019 Sam Lantinga <slouken@libsdl.org>
+  Copyright (C) 1997-2020 Sam Lantinga <slouken@libsdl.org>
 
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the authors be held liable for any damages
@@ -58,8 +58,6 @@
 #   else
 #    define DECLSPEC    __declspec(dllimport)
 #   endif
-#  elif defined(SDL_STATIC)
-#   define DECLSPEC
 #  else
 #   define DECLSPEC __declspec(dllexport)
 #  endif
