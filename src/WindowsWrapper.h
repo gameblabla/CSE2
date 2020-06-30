ccffba8a80e6b939e81868ad935f7cf770de4b7d
Clownacy <Clownacy@users.noreply.github.com>
2020-06-30 13:49:14 +0100

    Remove user-sabotage
    They could have their reasons.

--- src/WindowsWrapper.h
+++ src/WindowsWrapper.h
@@ -4,7 +4,6 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #undef FindResource
-#undef MAX_PATH	// Explicitly undefine MAX_PATH to avoid accidental usage of it
 #else
 
 #define RGB(r,g,b) ((r) | ((g) << 8) | ((b) << 16))
