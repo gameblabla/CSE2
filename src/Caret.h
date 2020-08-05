8adf5934036660e01c4a00128d2efa655eca9872
Clownacy <Clownacy@users.noreply.github.com>
2020-08-05 19:51:06 +0100

    Caret documentation/cleanup

--- src/Caret.h
+++ src/Caret.h
@@ -2,35 +2,6 @@
 
 #include "WindowsWrapper.h"
 
-#define CARET_MAX 0x40
-
-struct CARET_TABLE
-{
-	int view_left;
-	int view_top;
-};
-
-struct CARET
-{
-	int cond;
-	int code;
-	int direct;
-	int x;
-	int y;
-	int xm;
-	int ym;
-	int act_no;
-	int act_wait;
-	int ani_no;
-	int ani_wait;
-	int view_left;
-	int view_top;
-	RECT rect;
-};
-
-extern CARET gCrt[CARET_MAX];
-extern CARET_TABLE gCaretTable[18];
-
 void InitCaret(void);
 void ActCaret(void);
 void PutCaret(int fx, int fy);
