c396dede92534f60cfc603b1829db1386e25547d
Clownacy <Clownacy@users.noreply.github.com>
2020-10-14 20:03:01 +0100

    3DS - Take advantage of C++ default parameters

--- src/Backends/Rendering/3DS.cpp
+++ src/Backends/Rendering/3DS.cpp
@@ -184,7 +184,7 @@ void RenderBackend_DrawScreen(void)
 
 	SelectRenderTarget(screen_render_target);
 
-	C2D_DrawImageAt(image, (400 - framebuffer_surface->width) / 2, (240 - framebuffer_surface->height) / 2, 0.0f, NULL, 1.0f, 1.0f);
+	C2D_DrawImageAt(image, (400 - framebuffer_surface->width) / 2, (240 - framebuffer_surface->height) / 2, 0.0f);
 
 	EndRendering();
 }
@@ -334,7 +334,7 @@ void RenderBackend_Blit(RenderBackend_Surface *source_surface, const RenderBacke
 
 	SelectRenderTarget(destination_surface->render_target);
 
-	C2D_DrawImageAt(image, x, y, 0.0f, NULL, 1.0f, 1.0f);
+	C2D_DrawImageAt(image, x, y, 0.0f);
 }
 
 void RenderBackend_ColourFill(RenderBackend_Surface *surface, const RenderBackend_Rect *rect, unsigned char red, unsigned char green, unsigned char blue)
@@ -461,7 +461,7 @@ void RenderBackend_DrawGlyph(long x, long y, size_t glyph_x, size_t glyph_y, siz
 	image.tex = &glyph_atlas->texture;
 	image.subtex = &subtexture;
 
-	C2D_DrawImageAt(image, x, y, 0.0f, &glyph_tint, 1.0f, 1.0f);
+	C2D_DrawImageAt(image, x, y, 0.0f, &glyph_tint);
 }
 
 void RenderBackend_HandleRenderTargetLoss(void)
